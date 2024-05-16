c309d38b1911bbd476ee3c357a0cd5c435972b71
Saanju Priya <saanju.priyaak@gmail.com>
2024-05-16 00:17:12 -0400

    New details for Cognito authentication

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_vvXUG9tN3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'a2n1gb8va9ifcp1421vdijanc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_mK3odI7vZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6agq9otrf1kp09dpbf308s4n7g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
