b2dae9c938999a82755709fb7c655e6d2dcce72a
Saanju Priya <saanju.priyaak@gmail.com>
2024-05-16 00:07:59 -0400

    Added cognito user pool details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_vvXUG9tN3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'a2n1gb8va9ifcp1421vdijanc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
